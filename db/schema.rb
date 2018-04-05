e6727fd3389368dc55662d9b91707756a2ec39b6
Kazuhiro-M <km.91.h3@gmailcom>
2018-04-05 20:49:03 +0000

    Finish user destroy action

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180404083857) do
+ActiveRecord::Schema.define(version: 20180405203006) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20180404083857) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
